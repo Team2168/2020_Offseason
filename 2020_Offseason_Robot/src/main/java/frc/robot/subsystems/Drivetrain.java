1150fd135ddb16e474ad7852f3707a8f4402835d
Derek Osborne <legodmo10@gmail.com>
2019-12-12 19:55:11 -0500

    created drivetrain subsystem. 4 motors and method to control 1.

---
+++ 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  private VictorSP leftMotor1 = new VictorSP(0);
+  private VictorSP leftMotor2 = new VictorSP(1);
+  private VictorSP rightMotor1 = new VictorSP(2);
+  private VictorSP rightMotor2 = new VictorSP(3);
+
+  public void driveleftMotor1(double speed) {
+    leftMotor1.setSpeed(speed);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
