80b136ca4a7cd17e49f6e9833db6cd7eb180c1ef
Ian Weigle <ianeweig@grotonk12.org>
2019-12-16 19:57:12 -0500

    added more functions to drive different combos of motors easily

--- 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -13,13 +13,39 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * Add your docs here.
  */
-public class Drivetrain extends Subsystem{
+public class Drivetrain extends Subsystem {
     private VictorSP leftMotor1 = new VictorSP(0);
     private VictorSP leftMotor2 = new VictorSP(1);
     private VictorSP rightMotor1 = new VictorSP(2);
     private VictorSP rightMotor2 = new VictorSP(3);
 
-    public void driveLeftMotor1(double speedPercCHANGED) {
-        leftMotor1.setSpeed(speedPercCHANGED);
+    //next four functions allow you to set individual speeds for each motor
+    public void driveLeftMotor1(double speed) {
+        leftMotor1.setSpeed(speed);
+    }
+    public void driveLeftMotor2(double speed) {
+        leftMotor2.setSpeed(speed);
+    }
+    public void driveRightMotor1(double speed) {
+        rightMotor1.setSpeed(speed);
+    }
+    public void driveRightMotor2(double speed) {
+        rightMotor2.setSpeed(speed);
+    }
+
+    public void driveLeftSide(double speed) { //sets left side motors to same speed
+        driveLeftMotor1(speed);
+        driveLeftMotor2(speed);
+    }
+    public void driveRightSide(double speed) { //sets right side motors to same speed
+        driveRightMotor1(speed);
+        driveRightMotor2(speed);
+    }
+
+    public void driveAll(double speed) { //sets all motors to same speed
+        driveLeftMotor1(speed);
+        driveLeftMotor2(speed);
+        driveRightMotor1(speed);
+        driveRightMotor2(speed);
     }
 }
