8c1aaf88e030d9247daa7e5e8172663cd8b5f555
Nathan Kimpan <Nathankimpan@gmail.com>
2019-12-19 19:58:11 -0500

    Finnished Drivetrain Added to Robot. Created DriveWithConstant command.

--- 2020_Offseason_Robot/src/main/java/frc/robot/Robot.java
+++ 2020_Offseason_Robot/src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -26,6 +27,8 @@ public class Robot extends TimedRobot {
   public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
   public static OI m_oi;
 
+  public static DriveTrain driveTrain = DriveTrain.getInstance();
+ 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
--- 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,25 +13,67 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * An example subsystem. You can replace me with your own Subsystem.
  */
 public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-    private VictorSP leftMotor1 = new VictorSP(0);
-    private VictorSP leftMotor2 = new VictorSP(1);
-    private VictorSP rightMotor1 = new VictorSP(2);
-    private VictorSP rightMotor2 = new VictorSP(3);
+  private VictorSP leftMotor1;
+  private VictorSP leftMotor2;
+  private VictorSP rightMotor1;
+  private VictorSP rightMotor2;
+  
+  private static DriveTrain _instance = null;  
 
-    public void driveLeftMotor1(double speed) {
-        leftMotor1.setSpeed(speed);
+  private DriveTrain(){
+    leftMotor1 = new VictorSP(0);
+    leftMotor2 = new VictorSP(1);
+    rightMotor1 = new VictorSP(2);
+    rightMotor2 = new VictorSP(3);
+  }
+  
+  public static DriveTrain getInstance() {
+    if(_instance == null) {
+      _instance = new DriveTrain();
+
     }
+    return _instance;
+  }
+  public void driveleftMotor1(double speed) {
+    leftMotor1.setSpeed(speed);
+  }
 
+  public void driveleftMotor2(double speed) {
+    leftMotor2.setSpeed(speed);
+  }
 
+  public void driverightMotor1(double speed) {
+    rightMotor1.setSpeed(speed);
+  }
+
+  public void driverightMotor2(double speed) {
+    rightMotor2.setSpeed(speed);
+  }
+
+  public void driveleftMotors(double speed) {
+    driveleftMotor1(speed);
+    driveleftMotor2(speed);
+  }
+
+  public void driverightMotors(double speed) {
+    driverightMotor1(speed);
+    driverightMotor2(speed);
+  }
+  public void TAnkDRivE(double leftMotor, double rightMotor) {
+    driveleftMotors(leftMotor);
+    driverightMotors(rightMotor);
+  } 
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+    /// setDefaultCommand(new MySpecialCommand());
+ 
+ 
   }
 }
