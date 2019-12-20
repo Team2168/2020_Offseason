145f683b28d7a00b962e72bf787e05b9850785c0
Cooper-Halvordson <cjhalvord@gmail.com>
2019-12-19 19:57:55 -0500

    Finished Drivetrain. Added to Robot. Created DriveWithConstant command.

--- 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2020_Offseason_Robot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,20 +11,64 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * An example subsystem. You can replace me with your own Subsystem.
  */
 public class Drivetrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  private VictorSP leftMotor1 = new VictorSP(0);
-  private VictorSP leftMotor2 = new VictorSP(1);
-  private VictorSP rightMotor1 = new VictorSP(2);
-  private VictorSP rightMotor2 = new VictorSP(3);
+  private VictorSP leftMotor1;
+  private VictorSP leftMotor2;
+  private VictorSP rightMotor1;
+  private VictorSP rightMotor2;
+
+  private static Drivetrain _instance = null;
+
+  private Drivetrain() {
+    leftMotor1 = new VictorSP(0);
+    leftMotor2 = new VictorSP(1);
+    rightMotor1 = new VictorSP(2);
+    rightMotor2 = new VictorSP(3);
+  }
+
+  public static Drivetrain getInstance() {
+    if (_instance == null) {
+      _instance = new Drivetrain();
+    }
+    return _instance;
+  }
 
   public void driveLeftMotor1(double speed) {
     leftMotor1.setSpeed(speed);
   }
+
+  public void driveLeftMotor2(double speed) {
+    leftMotor2.setSpeed(speed);
+  }
+
+  public void driveRightMotor1(double speed) {
+    rightMotor1.setSpeed(speed);
+  }
+
+  public void driveRightMotor2(double speed) {
+    rightMotor2.setSpeed(speed);
+  }
+
+  public void driveLeftMotors(double speed) {
+    driveLeftMotor1(speed);
+    driveLeftMotor2(speed);
+  }
+
+  public void driveRightMotors(double speed) {
+    driveRightMotor1(speed);
+    driveRightMotor2(speed);
+  }
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    driveLeftMotors(leftSpeed);
+    driveRightMotors(rightSpeed);
+  }
+
 
   @Override
   public void initDefaultCommand() {
